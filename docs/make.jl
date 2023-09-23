421ca2ee5c7f0d522391b6bc2bec0c6f0bd36a34
Arno Strouwen <arno.strouwen@telenet.be>
2023-09-23 06:28:20 +0200

    Documenter 1.0 upgrade

--- docs/make.jl
+++ docs/make.jl
@@ -9,15 +9,6 @@ makedocs(sitename = "PoissonRandom.jl",
     authors = "Chris Rackauckas",
     modules = [PoissonRandom],
     clean = true, doctest = false, linkcheck = true,
-    strict = [
-        :doctest,
-        :linkcheck,
-        :parse_error,
-        :example_block,
-        :cross_references,
-        # Other available options are
-        # :autodocs_block, :cross_references, :docs_block, :eval_block, :example_block, :footnote, :meta_block, :missing_docs, :setup_block
-    ],
     format = Documenter.HTML(analytics = "UA-90474609-3",
         assets = ["assets/favicon.ico"],
         canonical = "https://docs.sciml.ai/PoissonRandom/stable/"),
